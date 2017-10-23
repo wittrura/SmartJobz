fbcebb26de767d79c2b635a2decfa0b38a6403ad
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-10-23 15:54:24 -0500

    refactor: moves db files to one folder, updates server.js to app.js

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8000;
+
+const companies = require('./routes/companies');
+
+var app = express();
+
+
+app.use(logger('dev'));
+
+// enable CORS for development
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  // define allowable methods - PATCH is not allowed by default
+  res.header("Access-Control-Allow-Methods", 'GET, POST, DELETE, PATCH');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/api/companies', companies);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}...`);
+});
+module.exports = app;
--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,38 @@
 // Update with your config settings.
+const path = require('path');
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/smartjobz_dev'
+    connection: 'postgres://localhost/smartjobz_dev',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
   },
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/smartjobz_test'
+    connection: 'postgres://localhost/smartjobz_test',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
   }
 };
